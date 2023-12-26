612418f7c9d61dd0c3e164a6371de5bdac5da412
Yury Frolov <yura200253@gmail.com>
2023-12-26 17:41:31 +0500

    Updated module name

--- example/service/s3/patchObject/patchObject.go
+++ example/service/s3/patchObject/patchObject.go
@@ -4,11 +4,11 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/endpoints"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/yezzey-gp/aws-sdk-go/aws"
+	"github.com/yezzey-gp/aws-sdk-go/aws/endpoints"
+	"github.com/yezzey-gp/aws-sdk-go/aws/session"
+	"github.com/yezzey-gp/aws-sdk-go/service/s3"
+	"github.com/yezzey-gp/aws-sdk-go/service/s3/s3manager"
 
 	"fmt"
 	"log"
